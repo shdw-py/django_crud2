f4dca16f142b7c13f129d0d923798a59e76a341f
delphiu-m <caioszrm1@gmail.com>
2023-09-21 17:39:00 -0300

    added update

--- djng_crud2/meuapp/models.py
+++ djng_crud2/meuapp/models.py
@@ -1,7 +1,9 @@
 # meuapp/models.py
 from django.db import models
+from django.urls import reverse
 
 class Produto(models.Model):
+    id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=100)
     descricao = models.TextField()
     preco = models.DecimalField(max_digits=10, decimal_places=2)
--- djng_crud2/meuapp/views.py
+++ djng_crud2/meuapp/views.py
@@ -1,42 +1,44 @@
 # produtos_files/views.py
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Produto
 from .forms import ProdutoForm
 
 def home(request):
     return render(request, 'produtos_files/home.html')
 
+# done
 def listar_produtos(request):
     produtos = {
         'produtos': Produto.objects.all()
     }
-    return render(request, 'produtos_files/lista_produtos.html', produtos)
+    return render(request, 'produtos_files/listar_produtos.html', produtos)
 
-
+# done
 def cria_produto(request):
     form = ProdutoForm() 
     if request.method == 'POST':
         form = ProdutoForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('lista_produtos')
+            return redirect('listar_produtos')
     return render(request, 'produtos_files/criar_formulario.html', {'form': form})
 
     
 def atualiza_produto(request, pk):
-    produto = Produto.objects.get(pk=pk)
+    produto = get_object_or_404(Produto, pk=pk)
     if request.method == 'POST':
         form = ProdutoForm(request.POST, instance=produto)
         if form.is_valid():
             form.save()
-            return redirect('lista_produtos')
-        else:
-            form = ProdutoForm(instance=produto)
-            return render(request, 'produtos_files/atualiza_produto.html', {'form': form})
+            return redirect('listar_produtos')
+    else:
+        form = ProdutoForm(instance=produto)
+    return render(request, 'produtos_files/atualiza_produto.html', {'form': form})
+
         
 def deleta_produto(request, pk):
-    produto = Produto.objects.get(pk=pk)
+    produto = get_object_or_404(Produto, pk=pk)
     if request.method == 'POST':
         produto.delete()
-        return redirect('lista_produtos')
+        return redirect('listar_produtos')
     return render(request, 'produtos_files/deleta_produto.html', {'produto': produto})
